ec4cc35819996c325865d8271e8393b6d867c39d
Pola-Ayaad <polaayaad@gmail.com>
2022-07-16 04:45:27 +0200

    sc

---
+++ security.tf
@@ -0,0 +1,54 @@
+# Allow SSH from the internet
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow ssh inbound traffic"
+  vpc_id      = aws_vpc.public-vpc.id
+
+  ingress {
+    description = "allow_ssh"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
+
+
+# Allow SSH from VPC cidr
+resource "aws_security_group" "allow_ssh_vpc" {
+  name        = "allow_ssh_vpc"
+  description = "Allow ssh inbound traffic_vpc"
+  vpc_id      = aws_vpc.public-vpc.id
+
+  ingress {
+    description = "allow_ssh_vpc"
+    from_port   = 22
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh_vpc"
+  }
+}
