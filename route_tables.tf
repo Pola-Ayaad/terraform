99445a86d74241872f29c164c6b46a5a8ba89f15
Pola-Ayaad <polaayaad@gmail.com>
2022-07-16 04:15:55 +0200

    latest

--- route_tables.tf
+++ route_tables.tf
@@ -32,7 +32,7 @@ resource "aws_route_table" "private" {
 }
 
 resource "aws_route" "private_route" {
-  route_table_id         = aws_route_table.public.id
+  route_table_id         = aws_route_table.private.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_nat_gateway.mynat.id
 
