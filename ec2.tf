24c82f1310464012d61702cc333ee98473e8924c
Pola-Ayaad <polaayaad@gmail.com>
2022-07-16 10:02:51 +0200

    final

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "bastion" {
-  ami             = "ami-0070c5311b7677678"
-  instance_type   = "t2.micro"
-  subnet_id       = aws_subnet.public_1.id
-  security_groups = [aws_security_group.allow_ssh.id]
-  key_name        = aws_key_pair.admin.key_name
+  ami                    = "ami-0070c5311b7677678"
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.public_1.id
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  key_name               = aws_key_pair.admin.key_name
 
   tags = {
     Name = "bastion"
@@ -12,11 +12,11 @@ resource "aws_instance" "bastion" {
 
 
 resource "aws_instance" "application" {
-  ami             = "ami-0070c5311b7677678"
-  instance_type   = "t2.micro"
-  subnet_id       = aws_subnet.private_1.id
-  security_groups = [aws_security_group.allow_ssh_vpc.id]
-  key_name        = aws_key_pair.admin.key_name
+  ami                    = "ami-0070c5311b7677678"
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.private_1.id
+  vpc_security_group_ids = [aws_security_group.allow_ssh_vpc.id]
+  key_name               = aws_key_pair.admin.key_name
 
   tags = {
     Name = "application"
